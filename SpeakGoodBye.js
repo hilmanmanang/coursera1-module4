f2b03a69f157f0487c4c6e2030a29d8daa2fcc8c
Hilman Ahmad <hilmanmanang@gmail.com>
2020-06-02 16:44:45 +0800

    end

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -14,14 +14,13 @@
 (function (window) {
 
   var byeSpeaker = {};
-  byeSpeaker.name = [];
   var speakWord = "Good Bye";
   window.byeSpeaker = byeSpeaker;
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  byeSpeaker.speak = function () {
-    console.log(speakWord + " " + byeSpeaker.name);
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
 })(window);
 
--- SpeakHello.js
+++ SpeakHello.js
@@ -10,15 +10,14 @@
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 (function (window) {
   var helloSpeaker = {};
-  helloSpeaker.name = [];
   var speakWord = "Hello";
   window.helloSpeaker = helloSpeaker;
 
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
   // helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  helloSpeaker.speak = function () {
-    console.log(speakWord + " " + helloSpeaker.name);
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
 })(window);
 
